8d1b861a06fbe8448081def2a879c2cb9916d323
SithijaSankalpa <kshithijasankalpa1234@gmail.com>
2025-12-04 22:31:11 +0530

    First commit

---
+++ client/src/components/WithdrawModal.jsx
@@ -0,0 +1,69 @@
+import { X } from 'lucide-react'
+import React, { useState } from 'react'
+
+const WithdrawModal = ({onClose}) => {
+  
+  const [amount, setAmount] = useState("")
+  const [account, setAccount] = useState([
+    {type: "text", name: "Account Holder Name", value: ""},
+    {type: "text", name: "Bank Name", value: ""},
+    {type: "number", name: "Account Number", value: ""},
+    {type: "text", name: "Account Type", value: ""},
+    {type: "text", name: "SWIFT/IFSC Code", value: ""},
+    {type: "text", name: "Branch", value: ""}
+  ])
+
+  const handleSubmisson = async (e) => {
+    e.preventDefault();
+    // Add your withdrawal logic here
+  }
+
+  return (
+    <div className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-100 flex items-center justify-center sm:p-4'>
+        <div className="bg-white sm:rounded-lg shadow-2xl w-full max-w-2xl sm:max-h-[90vh] flex flex-col">
+          {/* Header*/}
+          <div className='bg-gradient-to-r from-indigo-600 to-indigo-400 text-white p-4 sm:rounded-t-lg flex items-center justify-between'>
+              <div className='flex-1 min-w-0'>
+                <h3 className="font-semibold text-lg truncate">Withdraw Funds</h3>
+              </div>
+              <button onClick={onClose} className="ml-4 p-1 hover:bg-white/20 hover:bg-opacity-20 rounded-lg transition-colors">
+                <X className="w-5 h-5"/>
+              </button>
+          </div>
+          {/* Form*/}
+          <form onSubmit={handleSubmisson} className='flex flex-col items-start gap-4 p-4 overflow-y-auto'>
+            <div className="grid grid-cols-[2fr_3fr] items-center gap-2 w-full">
+                <label className="text-sm font-medium text-gray-800">Amount</label>
+                <input 
+                  onChange={(e) => setAmount(e.target.value)} 
+                  value={amount} 
+                  type='number' 
+                  className="w-full px-2 py-1.5 text-sm border border-gray-300 rounded outline-indigo-400" 
+                  required
+                />
+            </div>
+            {account.map((field, index)=>(
+              <div key={index} className="grid grid-cols-[2fr_3fr] items-center gap-2 w-full">
+                <label className="text-sm font-medium text-gray-800">{field.name}</label>
+                <input 
+                  type={field.type} 
+                  value={field.value} 
+                  onChange={(e)=>setAccount((prev)=>prev.map((c, i) => (i === index ? {
+                    ...c, value: e.target.value 
+                  } : c )))} 
+                  className='w-full px-2 py-1.5 text-sm border border-gray-300 rounded outline-indigo-400'
+                  required
+                />
+              </div>
+            ))}
+            {/*Submit button */}
+            <button type="submit" className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 mt-4 rounded-md">
+                Apply for withdrawal
+            </button>
+          </form>
+        </div>
+    </div>
+  )
+}
+
+export default WithdrawModal
